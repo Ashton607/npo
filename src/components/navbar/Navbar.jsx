ef5e7a8cf9977916a1f2c6847e37ff1f6845e880
Ashton607 <portmanashton4@gmail.com>
2026-07-04 12:56:43 +0200

    Installed: react router dom

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+    <ul>
+        <li>Home</li>
+        <li>our work</li>
+        <li>programs</li>
+        <li>contact</li>
+        <li>donate</li>
+    </ul>
+    </nav>
+  )
+}
+
+export default Navbar
