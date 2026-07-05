a97930db03c1dab5ca9ba6e569c438606be81816
Ashton607 <portmanashton4@gmail.com>
2026-07-05 10:41:21 +0200

    Added: our work preview on home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Home from './components/home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Hero/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import './Home.css'
-import Hero from '../hero/Hero'
+import preview1 from '../../assets/previewImg1.jpg'
+import preview2 from '../../assets/previewImg2.jpg'
+import preview3 from '../../assets/previewImg3.jpg'
+
 
 const Home = () => {
   return (
     <div className='Home'>
-    
     <div className="home-content">
+
+    <span className="home-badge">what we do for you</span>
+
+    <h2 className="home-about-title">For Corporate & Funding Partners</h2>
     
-    <div className="hero-content">
-    <Hero/>
+    <p className="home-about-subtitle">Empowering your Corporate Social Investment to create measurable, 
+    dignified change in our communities</p>
+
+    <div className="home-about-preview">
+      <div className="preview-card">
+
+        <div className="preview-img1">
+        <img src={preview1} alt="Preview 1" />
+        <div className="preview-overlay">
+          <span className="preview-title">Preview Title</span>
+          <span className="preview-subtitle">Preview Subtitle</span>
+        </div>
+        </div>
+
+        <div className="preview-img2">
+        <img src={preview2} alt="Preview 2" />
+        <div className="preview-overlay">
+          <span className="preview-title">Preview Title</span>
+          <span className="preview-subtitle">Preview Subtitle</span>
+        </div>
+        </div>
+
+        <div className="preview-img3">
+        <img src={preview3} alt="Preview 3" />
+        <div className="preview-overlay">
+          <span className="preview-title">Preview Title</span>
+          <span className="preview-subtitle">Preview Subtitle</span>
+        </div>
+        </div>
+
+      </div>
     </div>
+
+
 
 
     </div>
