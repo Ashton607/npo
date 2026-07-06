b393cf8b8bcfb26309eb3494153443dafbbf4731
Ashton607 <portmanashton4@gmail.com>
2026-07-06 19:36:25 +0200

    Added: programmes dropdown menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,7 +26,22 @@ const Navbar = () => {
     <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/ourwork">our work</Link></li>
-        <li><Link to="/programmes">programmes</Link></li>
+        <li className="nav-item has-dropdown">
+        <Link to="/programmes">
+        Programmes <span className="dropdown-arrow">▾</span>
+        </Link>
+        <ul className="dropdown-menu">
+        <li>
+        <Link to="/programmes/relief">Immediate Relief Program</Link>
+        </li>
+        <li>
+        <Link to="/programmes/dignity">Dignity Program</Link>
+        </li>
+        <li>
+        <Link to="/programmes/outreach">Community Outreach Program</Link>
+        </li>
+        </ul>
+        </li>
         <li><Link to="/contact">contact</Link></li>
         <li><Link to="/donate">donate</Link></li>
     </ul>
